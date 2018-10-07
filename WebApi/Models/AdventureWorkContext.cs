df98c483f4eb80755c4889af7b947f8e16e32c4b
MINSK\Aliaksandr_Dzemidovi <Aliaksandr.Dzemidovich@thomsonreuters.com>
2018-10-07 20:22:45 +0300

    Add project files.

---
+++ WebApi/Models/AdventureWorkContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApi.Models
+{
+    public partial class AdventureWorkContext : DbContext
+    {
+        public AdventureWorkContext()
+        {
+        }
+
+        public AdventureWorkContext(DbContextOptions<AdventureWorkContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=EPBYMINW5083;Database=AdventureWorks2017;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+               
+            });
+        }
+    }
+}
---
+++ WebApi/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Models
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+    }
+}
